37dd6f67bd4b410b8fb45ba8510ec8260415d79a
myweb1 <119072006+myweb1@users.noreply.github.com>
2024-02-02 09:17:45 -0800

    Add files via upload

---
+++ main.js
@@ -0,0 +1,10 @@
+function dunction() {
+    document.getElementById("hello").innerHTML = puntos
+    puntos -= 1
+}
+
+var puntos = 1000000
+
+if (puntos == 0 ) {
+    document.getElementById("k").innerHTML = "ganastee!!!!! felicidadess"
+}
